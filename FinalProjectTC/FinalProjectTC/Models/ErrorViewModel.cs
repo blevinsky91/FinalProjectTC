237de8a651ffbfffac21dfb155306eaef3d966d2
Brandon Levinsky <blevinsky91@gmail.com>
2023-08-03 12:31:46 -0400

    set up

---
+++ FinalProjectTC/FinalProjectTC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProjectTC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
