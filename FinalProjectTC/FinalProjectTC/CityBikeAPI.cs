065d6198b0ef1a60e0864776dca6137bbb696699
Brandon Levinsky <blevinsky91@gmail.com>
2023-08-07 12:26:16 -0400

    save4

---
+++ FinalProjectTC/FinalProjectTC/CityBikeAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FinalProjectTC.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FinalProjectTC
+{
+	public class CityBikeAPI
+	{
+	    static async Task Main()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string apiUrl = "http://api.citybik.es/v2/networks";
+                    HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        Network networksResponse = JsonConvert.DeserializeObject<Network>(jsonResponse);
+
+                        
+                        foreach (Network network in networksResponse.networks)
+                        {
+                            Console.WriteLine($"ID: {network.id}, Name: {network.name}, Company: {network.company}, Location: {network.location}");
+                            
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"API request failed with status code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+}
+
+
--- FinalProjectTC/FinalProjectTC/Models/Network.cs
+++ FinalProjectTC/FinalProjectTC/Models/Network.cs
@@ -6,11 +6,11 @@ namespace FinalProjectTC.Models
         public object company { get; set; }
         public string href { get; set; }
         public string id { get; set; }
-        public Location location { get; set; }
+        public string location { get; set; }
         public string name { get; set; }
         public string source { get; set; }
         public string gbfs_href { get; set; }
-        public License license { get; set; }
+        public string License { get; set; }
         public bool? ebikes { get; set; }
     }
 }
